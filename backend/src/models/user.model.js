12c9a4064a69cf6c17d7b6abb53a5eb8af09220c
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-09-28 13:55:24 +0530

    user register api

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 11,
+    },
+    password: {
+        type: String,
+        minlength: 6
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+const userModel = mongoose.model('user', userSchema);
+module.exports = userModel
