19b14f57d3cd6cb0070b795777e22df463ac17b2
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-09-29 12:08:04 +0530

    food partner login & logout api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -119,9 +119,54 @@ async function registerFoodPartner(req, res) {
     });
 }
 
+async function loginFoodPartner(req, res) {
+
+    const { email, password } = req.body;
+
+    const foodPartner = await foodPartnerModel.findOne({ email });
+
+    if (!foodPartner) {
+        return res.status(400).json({
+            message: "Invalid email or password",
+        });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, foodPartner.password);
+
+    if (!isPasswordValid) {
+        return res.status(400).json({
+        message: "Invalid email or password",
+        });
+    }
+
+    const token = jwt.sign({
+        id: foodPartner._id,
+    },process.env.JWT_SECRET);
+
+    res.cookie("token", token);
+
+    res.status(201).json({
+        message: "Food Partner login successfully",
+        foodPartner: {
+            _id: foodPartner._id,
+            fullName: foodPartner.fullName,
+            email: foodPartner.email,
+        },
+    });
+}
+
+async function logoutFoodPartner(req, res) {
+    res.clearCookie("token");
+    res.status(200).json({
+        message: "Food Partner logged out successfully",
+    });
+}
+
 module.exports = {
     registerUser,
     loginUser,
     logoutUser,
-    registerFoodPartner
+    registerFoodPartner,
+    loginFoodPartner,
+    logoutFoodPartner,
 }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -7,5 +7,7 @@ router.post('/user/register', authController.registerUser)
 router.post('/user/login', authController.loginUser)
 router.get('/user/logout', authController.logoutUser)
 router.post('/foodpartner/register', authController.registerFoodPartner)
+router.post('/foodpartner/login', authController.loginFoodPartner)
+router.get('/foodpartner/logout', authController.logoutFoodPartner)
 
 module.exports = router;
