36e4527e481c7a8921e11c632ebf4de53d799abd
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-09-30 11:58:30 +0530

    food get api

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -19,6 +19,16 @@ async function createFood(req, res) {
     })
 }
 
+async function getFoodItems(req, res) {
+    const foodItems = await foodModel.find({})
+
+    res.status(200).json({
+        message: "Food items fetched sucessfully",
+        foodItems
+    })
+}
+
 module.exports = {
-    createFood
+    createFood,
+    getFoodItems
 }
